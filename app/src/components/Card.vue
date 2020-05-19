6e0d9207bbaaf1fa4b6be85306b0338183014d79
Arianny Janse <arianny.janse@gmail.com>
2020-05-19 18:14:54 -0500

    Adding the first version of web interface

---
+++ app/src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <b-card border-variant="dark" header="Dark" align="center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+            aaaaa<br>
+            aaaaa<br>
+            asdasds<br>
+            displaydsad<br>
+            ssdsds<br>
+            asdasds<br>
+            asdsdsd<br>
+            dssadsd<br>
+            adsa
+        </b-card-text>
+    </b-card>   
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+.card {
+    display: inline-block;
+    width: 40%;
+    margin: 3% 0 5% 0;
+}
+</style>
